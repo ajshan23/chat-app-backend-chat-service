346ecac29ba82898bef3311b298447f92a223c83
ajshan23 <ajmalshahan23@gmail.com>
2024-11-21 00:36:47 +0530

    message typing ,seen functionality done

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 import express from "express";
 import http from 'http';
 import Redis from 'ioredis';
+import Message from "../models/messageModel";
 
 
 const app = express();
@@ -47,6 +48,43 @@ io.on('connection', async (socket) => {
     const users = await redis.hkeys('userSocketMap');
 
     io.emit('getOnlineUsers', users);
+
+    socket.on('typing', async (data) => {
+        const { to } = data; // recipient's userId
+        const recipientSocketId = await redis.hget('userSocketMap', to); // Fetch recipient's socketId
+
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('userTyping', { from: socket.handshake.query.userId });
+            // Notify User B (recipient) that User A is typing
+        }
+    })
+    socket.on('stopTyping', async (data) => {
+        const { to } = data; // recipient's userId
+        const recipientSocketId = await redis.hget('userSocketMap', to); // Fetch recipient's socketId
+
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('userTypingStopped', { from: socket.handshake.query.userId });
+            // Notify User B (recipient) that User A is typing
+        }
+    })
+    socket.on("messageSeen", async ({ messageId, conversationId, senderId }, callback) => {
+        // Process the messageId (e.g., mark message as seen in the database)
+        console.log("seeneeee");
+        const updateMessage = await Message.findByIdAndUpdate(messageId, {
+            isRead: true
+        }, { new: true })
+
+        if (updateMessage) {
+            const recipientSocketId: any = await redis.hget('userSocketMap', senderId);
+            io.to(recipientSocketId).emit("messageSeenNotification", { messageId, conversationId });
+            // Send acknowledgment back to the client
+            callback({
+                status: "success",  // or failure status if something went wrong
+                messageId: messageId // Optional, you can return the messageId back
+            });
+        }
+    });
+
 
     socket.on("disconnect", async () => {
         console.log('USER DISCONNECTED', socket.id);
